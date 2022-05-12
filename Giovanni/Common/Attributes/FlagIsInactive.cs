9584b3577d8050214639d39622895383932a8b1c
Sebastián <sebastian@starck.im>
2022-05-12 19:57:40 -0300

    Untracked ignored files

---
+++ Giovanni/Common/Attributes/FlagIsInactive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Giovanni.Common.Attributes
+{
+    public class FlagIsInactive : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
