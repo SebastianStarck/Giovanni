0f6dd25cb14a79f6e2fa1cd25ffb87a02572dce9
Sebastián <sebastian@starck.im>
2022-05-16 11:12:40 -0300

    Added command argument parser

---
+++ Giovanni/Common/CommandArguments.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Giovanni.Common
+{
+    public abstract class CommandArguments
+    {
+        protected readonly Dictionary<string, string> Arguments;
+
+        protected CommandArguments(string[] arguments)
+        {
+            Arguments = new Dictionary<string, string>();
+
+            foreach (var argument in arguments)
+            {
+                var (key, value) = argument.Split('=');
+                Console.WriteLine($"{key}, {value}");
+                Arguments.Add(key, value ?? "true");
+            }
+        }
+
+        protected bool GetBoolValue(string key, bool defaultValue = false)
+        {
+            var value = Arguments.GetValueOrDefault(key, null);
+
+            return value is not null ? bool.Parse(value) : defaultValue;
+        }
+    }
+}
---
+++ Giovanni/Modules/Spotify/PurgeAsyncParams.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Giovanni.Common;
+
+namespace Giovanni.Modules.Spotify
+{
+    public class PurgeAsyncParams : CommandArguments
+    {
+        public PurgeAsyncParams(string[] arguments) : base(arguments)
+        {
+        }
+
+        public void Deconstruct(out bool disposeBotMessage, out string prefix, out bool botOnly)
+        {
+            disposeBotMessage = GetBoolValue("disposeBotMessage", false);
+            prefix = Arguments.GetValueOrDefault("prefix", "");
+            botOnly = GetBoolValue("botOnly");
+        }
+    }
+}
